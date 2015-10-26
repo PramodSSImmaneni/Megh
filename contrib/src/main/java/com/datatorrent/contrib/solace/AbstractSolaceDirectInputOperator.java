d80c8a847ab3feaa99a4d32b4c774a3d0c7d3e57
Pramod Immaneni <pramod@datatorrent.com>
2015-10-26 07:36:55 -0700

    Added documentation and copyright

--- contrib/src/main/java/com/datatorrent/contrib/solace/AbstractSolaceDirectInputOperator.java
+++ contrib/src/main/java/com/datatorrent/contrib/solace/AbstractSolaceDirectInputOperator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2015 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.datatorrent.contrib.solace;
 
 import java.io.IOException;
@@ -14,7 +30,8 @@ import com.datatorrent.api.Operator;
 import com.datatorrent.netlet.util.DTThrowable;
 
 /**
- * Created by pramod on 8/24/15.
+ * Operator to read data from a topic in a solace appliance.
+ * Multiple partitions can be used to read data parallely from the same topic.
  */
 public abstract class AbstractSolaceDirectInputOperator<T> extends AbstractSolaceBaseInputOperator<T> implements InputOperator, Operator.ActivationListener<com.datatorrent.api.Context.OperatorContext>
 {
--- contrib/src/main/java/com/datatorrent/contrib/solace/AbstractSolaceGuaranteedInputOperator.java
+++ contrib/src/main/java/com/datatorrent/contrib/solace/AbstractSolaceGuaranteedInputOperator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2015 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datatorrent.contrib.solace;
 
 import java.io.IOException;
@@ -18,7 +33,8 @@ import com.datatorrent.api.Operator;
 import com.datatorrent.netlet.util.DTThrowable;
 
 /**
- *
+ * Operator to read data from an endpoint in a solace appliance.
+ * Multiple partitions can be used to read data parallely from the same endpoint.
  */
 public abstract class AbstractSolaceGuaranteedInputOperator<T> extends AbstractSolaceBaseInputOperator<T> implements InputOperator, Operator.ActivationListener<Context.OperatorContext>
 {
